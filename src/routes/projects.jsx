d1e4915e56febbc2db87cc0a68e03be4ba482ec3
Caroline Leopold <caroline.leopold@gmail.com>
2022-01-11 16:53:09 -0500

    start project data and route

--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
+import { getProjects } from "../data";
+
 export default function Projects() {
+  let projects = getProjects();
   return (
-    <main>
-      <h2>Projects</h2>
-    </main>
-  )
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem"
+        }}
+      >
+        {projects.map(project => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            to={`/projects/${project.number}`}
+            key={project.number}
+          >
+            {project.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
 }
 
 
