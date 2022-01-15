f1d55a95f55f8dc7aa29a30c76714de2fa53c1d2
Caroline Leopold <caroline.leopold@gmail.com>
2022-01-15 12:42:52 -0500

    edit project categories

--- src/data.js
+++ src/data.js
@@ -6,25 +6,25 @@ let projects = [
     due: "12/05/1995"
   },
   {
-    name: "Stankonia",
+    name: "Grant Writing",
     number: 2000,
     amount: "$8,000",
     due: "10/31/2000"
   },
   {
-    name: "Ocean Avenue",
+    name: "Websites",
     number: 2003,
     amount: "$9,500",
     due: "07/22/2003"
   },
   {
-    name: "Tubthumper",
+    name: "Branding",
     number: 1997,
     amount: "$14,000",
     due: "09/01/1997"
   },
   {
-    name: "Wide Open Spaces",
+    name: "Interviews",
     number: 1998,
     amount: "$4,600",
     due: "01/27/2998"
