06749bba1191c6f67399e967fd192388118a3f0f
Caroline Leopold <caroline.leopold@gmail.com>
2022-01-17 14:16:12 -0500

    basic home landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 // import { SideNav } from './components/sideNav'
+import './global.css';
 import './App.css';
 
 function App() {
   return (
     <>
-    <div>
-      <h1>Portfolio</h1>
-      <Outlet />
+    <div className="Container">
+      <div className="Container-text">
+      <h1>Connected</h1>
+      <h2>I'm Caroline Leopold and I help audiences grasp health, research and technology.</h2>
+      </div>
+      <div className="Container-btn">
+        <Link to="/projects">
+        <button className="btn-cta">Portfolio</button>
+        </Link>
+      </div>
     </div> 
     </>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
+import './global.css';
 import App from './App';
 import Projects from './routes/projects';
 import Project from './routes/project'
